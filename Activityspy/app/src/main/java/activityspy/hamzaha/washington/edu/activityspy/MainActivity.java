b8347068cb6339ab44b7026a588801b0cf4ac0ca
hamzahaly <hzhaly93@gmail.com>
2017-01-13 13:52:45 -0800

    adds logs for i and e

--- Activityspy/app/src/main/java/activityspy/hamzaha/washington/edu/activityspy/MainActivity.java
+++ Activityspy/app/src/main/java/activityspy/hamzaha/washington/edu/activityspy/MainActivity.java
@@ -2,12 +2,22 @@ package activityspy.hamzaha.washington.edu.activityspy;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
+
+    private static final String TAG = "MyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate event fired.");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "We're going down, Captain!");
     }
 }
