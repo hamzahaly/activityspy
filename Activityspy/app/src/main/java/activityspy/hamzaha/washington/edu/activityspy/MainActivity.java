a6f37e06960ee68243b492a19bd8e6c1b6017ad8
hamzahaly <hzhaly93@gmail.com>
2017-01-13 13:45:16 -0800

    starter files

---
+++ Activityspy/app/src/main/java/activityspy/hamzaha/washington/edu/activityspy/MainActivity.java
@@ -0,0 +1,13 @@
+package activityspy.hamzaha.washington.edu.activityspy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
